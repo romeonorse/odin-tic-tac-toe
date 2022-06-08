aaab89e0a54be64086931bab6bb24d3efffd22da
romeonorse <romeonorse@gmail.com>
2022-06-08 21:31:21 +0300

    Complete player switch

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const gameBoard = (() => {
   const gbArr = ['', '', '', '', '', '', '', '', ''];
   const cells = document.querySelectorAll('.cell');
@@ -9,38 +11,56 @@ const gameBoard = (() => {
   }
   
   const players = document.querySelectorAll('.player');
-  
   const toggler = () => {
       players.forEach(player => player.classList.toggle('active'));
       console.log('toggler');
     }
+
+  const player1 = document.querySelector('.player-1');
+  const player2 = document.querySelector('.player-2');
+
+  let counter = 2;
   
   return {
     gbArr,
     cells,
     renderBoard,
-    toggler
+    toggler,
+    counter
   }
 })();
 
 
+
 const player = () => {
   
+  this.sign = 'O';
   let playerBoard = [...gameBoard.gbArr];
+  
+  const checkCounter = () => {
+    if (gameBoard.counter % 2 === 0) {
+      sign = 'X'
+    } else {
+      sign = 'O';
+    }
 
-  const markCell = function(sign) {
+    return sign;
+  }
+  
+  const markCell = function() {
     gameBoard.cells.forEach((cell, index) => {
       cell.addEventListener('click', function(e) {
-
+        checkCounter();
         if (e.target.innerText === '') {
           e.target.innerText = `${sign}`;
           playerBoard[index] = `${sign}`;
-          console.log(playerBoard);
           gameBoard.toggler();
+          return gameBoard.counter++;
         }
       })
     })
   }
+
   
   
   return {
@@ -48,5 +68,13 @@ const player = () => {
   }
 }
 
-const playerOne = player();
-const playerTwo = player();
+const currentPlayer = player();
+
+
+
+currentPlayer.markCell();
+
+
+
+
+
